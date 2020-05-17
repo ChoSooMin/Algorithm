733ecbf4af75020ba80015f3c98dd3e292e01182
ChoSooMin <vywlgns1102@naver.com>
2020-05-17 18:17:24 +0900

    [solved] BOJ_1937

--- Baekjoon/Dynamic Programming/BOJ_1937.cpp
+++ Baekjoon/Dynamic Programming/BOJ_1937.cpp
@@ -18,8 +18,7 @@ int n; // 대나무 숲 크기
 int forest[MAX][MAX];
 int dx[4] = { -1, 1, 0, 0 }; // 상, 하, 좌, 우
 int dy[4] = { 0, 0, -1, 1}; // 상, 하, 좌, 우
-//int min_num, min_x, min_y;
-//int min_x = 0, min_y = 0;
+int dp[MAX][MAX]; // dp 저장공간..
 
 // 대나무 숲 입력받기
 void input()
@@ -29,77 +28,48 @@ void input()
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
         {
+            dp[i][j] = 1; // dp를 1로 초기화해둔다.
             cin >> forest[i][j];
         }
 }
 
-int dp(int x, int y)
+int get_day(int x, int y)
 {
-    int answer = 1; // 1일부터 시작
-    int next_x = -1;
-    int next_y = -1;
-    int min = BAMBOO + 1;
-    int min_x = -1, min_y = -1;
+    int &day = dp[x][y];
     
-    // (상, 하, 좌, 우)에서 현재 대나무 양보다 크고, 제일 적은 곳으로..? 이동할 곳을 찾는다.
+    if (day != 1)
+        return day;
+    
     for (int i = 0; i < 4; i++)
     {
-        // (상, 하, 좌, 우)로 이동할 좌표 구하기
-        next_x = x + dx[i];
-        next_y = y + dy[i];
+        int next_x = x + dx[i];
+        int next_y = y + dy[i];
         
-        // 1. 이동할 좌표가 범위를 벗어나거나
-        // 2. 현재 좌표의 대나무 양보다 이동할 좌표의 대나무 양이 작으면
-        // 다음으로 넘어간다.
-        if (next_x < 0 || next_x >= n || next_y < 0 || next_y >= n || (forest[next_x][next_y] < forest[x][y]))
+        // next_x와 next_y가 범위를 벗어난다면 continue문 수행
+        if (next_x < 0 || next_x >= n || next_y < 0 || next_y >= n)
             continue;
         
-        if (min > forest[next_x][next_y])
+        // 다음 위치의 대나무 양이 현재 위치의 대나무 양보다 클 떄만 다음을 수행한다.
+        if (forest[next_x][next_y] > forest[x][y])
         {
-            min = forest[next_x][next_y];
-            min_x = next_x;
-            min_y = next_y;
+            // 메모이제이션 사용!
+            int next_dynamic = get_day(next_x, next_y) + 1; // 다음으로 이동해서 그 자리에서 또 get_day를 한다.
+            day = max(day, next_dynamic); // day와 next_dynamic 중 더 큰 값을 day에 넣어준다.
         }
     }
     
-    // 끝났을 때 min_x랑 min_y가 -1이라면 이동할 곳이 없다는 것 ..
-    if (min_x == -1 && min_y == -1)
-    {
-        int nextnext_x = -1, nextnext_y = -1;
-        
-        if (x == 3 && y == 3)
-        {
-            cout << "모르게따";
-        }
-        else if (y == 3)
-        {
-            nextnext_x = x + 1;
-            nextnext_y = 0;
-        }
-        else
-        {
-            nextnext_x = x + 1;
-            nextnext_y = y + 1;
-        }
-        dp(nextnext_x, nextnext_y);
-    }
-    else
-    {
-        answer++;
-        dp(min_x, min_y);
-    }
-    
-    
-    return answer;
+    return day;
 }
 
 int get_answer()
 {
     int answer = 0;
     
+    // 각각의 위치에서 dynamic_programming을 다 해본다.
+    // 그 중 제일 큰 값이 답
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
-            answer = max(answer, dp(i, j));
+            answer = max(answer, get_day(i, j)); // 매번 answer와 (i, j) 위치에서의 dynamic_programming 값을 비교하여 더 큰 값을 answer로 바꿔준다.
     
     return answer;
 }
