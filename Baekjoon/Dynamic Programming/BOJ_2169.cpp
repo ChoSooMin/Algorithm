d67c69fc732f0eca3f8413ca3f0cba6652f79276
ChoSooMin <vywlgns1102@naver.com>
2020-05-18 21:48:19 +0900

    [unsolved] BOJ_2169

--- Baekjoon/Dynamic Programming/BOJ_2169.cpp
+++ Baekjoon/Dynamic Programming/BOJ_2169.cpp
@@ -7,84 +7,65 @@
 //
 //  로봇 조종하기(https://www.acmicpc.net/problem/2169)
 
-#include <stdio.h>
 #include <iostream>
-#define MAX 1000 // 1 <= N, M <= 1000
+#define MAX 1001 // 1 <= N, M <= 1000
 
 using namespace std;
 
 int n, m;
-int mars[MAX][MAX];
-int dp[MAX][MAX]; // (1, 1)에서부터 (i, j)까지 이동했을 때 합이 최대가 되는 값들을 저장..?
-int dx[3] = { 0, 0, 1 }; // (왼쪽, 오른쪽, 아래쪽)
-int dy[3] = { -1, 1, 0 }; // (왼쪽, 오른쪽, 아래쪽)
+int mars[MAX][MAX] = { 0 };
+int dp[MAX][MAX] = { 0 }; // (1, 1)에서부터 (i, j)까지 이동했을 때 합이 최대가 되는 값들을 저장
+int leftRightCompare[2][MAX] = { 0 };
 
 void input()
 {
-    cin >> n >> m;
+    cin >> n >> m; // n,m 입력받기
     
-    for (int i = 0; i <=n; i++)
-        for (int j = 0; j <= m; j++)
-        {
-            dp[i][j] = 0;
-            
-            if (i == 0 || j == 0)
-                mars[i][j] = 0;
-            else if (i == 1 && j == 1)
-            {
-                cin >> mars[i][j];
-                dp[i][j] = mars[i][j]; // dp[1][1]에만 mars[1][1]과 같은 값을 넣어둔다.
-            }
-            else
-                cin >> mars[i][j];
-        }
+    // mars[i][j]를 입력받는다.
+    for (int i = 1; i <=n; i++)
+        for (int j = 1; j <= m; j++)
+            cin >> mars[i][j];
+    
+    // (1, 1)에서 시작하므로 (1, 1)은 꼭 들어가야 하는 값
+    dp[1][1] = mars[1][1]; // dp[1][1]은 mars[1][1]과 같은 값으로 초기화해둔다.
 }
 
-int dynamic_programming(int x, int y)
+int getMax()
 {
-    int now_answer = dp[x][y];
+    // i = 1일 때
+    for (int j = 2; j <= m; j++)
+        dp[1][j] = mars[1][j] + dp[1][j - 1]; // += 안해줘도 되나?
     
-    // 오른족 아래(n, m)에 도착하면 (n, m)위치의 값을 더해 그 값을 return 한다.
-    if (x == n && y == m)
-        return now_answer + mars[x][y];
-    
-    for (int i = 0; i < 3; i++)
+    // i = 2~n일 때
+    // leftRightCompare[0][j]는 왼쪽 -> 오른쪽으로 이동했을 때
+    // leftRightCompare[1][j]는 오른쪽 -> 왼쪽으로 이동할 때
+    for (int i = 2; i <= n; i++)
     {
-        int next_x = x + dx[i];
-        int next_y = y + dy[i];
+        leftRightCompare[0][0] = dp[i - 1][1]; // ?
         
-        // 범위를 벗어나면 아래 구문을 수행하지 않고 다음번으로 넘어간다.
-        if (next_x <= 0 || next_x > n || next_y <= 0 || next_y > m)
-            continue;
+        for (int j = 1; j <= m; j++)
+            leftRightCompare[0][j] = max(leftRightCompare[0][j - 1], dp[i - 1][j] + mars[i][j]);
         
-        // 오른족 아래(n, m)에 도착하면 (n, m)위치의 값을 더해 그 값을 return 한다.
-        if (next_x == n && next_y == m)
-            return now_answer + mars[next_x][next_y];
+        leftRightCompare[1][m + 1] = dp[i - 1][m];
         
-        // 제대로 된 위치라면 ..
-        // 이동해서 또 수행 ..?
-//        int next_dynamic = dynamic_programming(next_x, next_y);
-//        now_answer = max(now_answer, next_dynamic);
-//        dp[next_x][next_y] = now_answer;
-        dp[next_x][next_y] = max(now_answer, now_answer + dynamic_programming(next_x, next_y));
-//        dynamic_programming(next_x, next_y);
+        for (int j = m; j >= 1; j--)
+            leftRightCompare[1][j] = max(leftRightCompare[1][j + 1], dp[i - 1][j] + mars[i][j]);
+        
+        for (int j = 1; j <= m; j++)
+            dp[i][j] = max(leftRightCompare[0][j], leftRightCompare[1][j]);
+        
     }
+        
     
-    return now_answer;
-}
-
-int max_answer()
-{
-    int answer = dynamic_programming(1, 1);
-    
-    return answer;
+    return dp[n][m];
 }
 
 int main()
 {
     input();
     
-    cout << max_answer() << endl;
+    cout << getMax() << endl;
     
     return 0;
 }
+
