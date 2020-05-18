935531b6e7d7dc5059468fc5b3b7b86ba82af4e6
ChoSooMin <vywlgns1102@naver.com>
2020-05-18 22:12:04 +0900

    [solved] add explanation

--- Baekjoon/Dynamic Programming/BOJ_2169.cpp
+++ Baekjoon/Dynamic Programming/BOJ_2169.cpp
@@ -33,30 +33,35 @@ void input()
 int getMax()
 {
     // i = 1일 때
+    // 이미 지나온 길은 다시 가지 못하므로, 오른쪽으로 갈 수밖에 없다.
     for (int j = 2; j <= m; j++)
-        dp[1][j] = mars[1][j] + dp[1][j - 1]; // += 안해줘도 되나?
+        dp[1][j] = mars[1][j] + dp[1][j - 1];
     
     // i = 2~n일 때
-    // leftRightCompare[0][j]는 왼쪽 -> 오른쪽으로 이동했을 때
-    // leftRightCompare[1][j]는 오른쪽 -> 왼쪽으로 이동할 때
+    // case 1) leftRightCompare[0][j]는 왼쪽 -> 오른쪽으로 이동했을 때
+    // case 2) leftRightCompare[1][j]는 오른쪽 -> 왼쪽으로 이동할 때
+    // case 1과 case 2의 값을 비교해 더 큰 값을 dp[i][j]에 넣는다.
     for (int i = 2; i <= n; i++)
     {
-        leftRightCompare[0][0] = dp[i - 1][1]; // ?
+        // case 1) 왼쪽 -> 오른쪽 이동했을 때
+        leftRightCompare[0][0] = dp[i - 1][1]; // 제일 처음 값은 위에서 내려온 값
         
+        // 왼쪽 -> 오른쪽 오는 값 vs 위에서 내려오는 값 비교, 큰 값을 넣어준다.
         for (int j = 1; j <= m; j++)
             leftRightCompare[0][j] = max(leftRightCompare[0][j - 1], dp[i - 1][j]) + mars[i][j];
         
-        leftRightCompare[1][m + 1] = dp[i - 1][m];
+        // case 2) 오른쪽 -> 왼쪽 이동했을 때
+        leftRightCompare[1][m + 1] = dp[i - 1][m]; // 제일 마지막 값은 위에서 내려온 값
         
+        // 오른쪽 -> 왼쪽 오는 값 vs 위에서 내려오는 값 비교, 큰 값을 넣어준다.
         for (int j = m; j >= 1; j--)
             leftRightCompare[1][j] = max(leftRightCompare[1][j + 1], dp[i - 1][j]) + mars[i][j];
         
+        // case 1과 case 2 값 비교
         for (int j = 1; j <= m; j++)
             dp[i][j] = max(leftRightCompare[0][j], leftRightCompare[1][j]);
-        
     }
         
-    
     return dp[n][m];
 }
 
