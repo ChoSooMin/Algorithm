e202ccf1b96395239a4ceedec435fa5e56850519
ChoSooMin <vywlgns1102@naver.com>
2020-06-17 15:13:37 +0900

    add BOJ_1057

---
+++ Baekjoon/Math/BOJ_1057.cpp
@@ -0,0 +1,17 @@
+//
+//  BOJ_1057.cpp
+//  Algorithm
+//
+//  Created by 조수민 on 2020/06/17.
+//  Copyright © 2020 조수민. All rights reserved.
+//
+
+#include <stdio.h>
+
+using namespace std;
+
+
+int main()
+{
+    return 0;
+}
