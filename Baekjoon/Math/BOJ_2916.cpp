01dc10c73238dfee1595050b8338e817f84af2de
ChoSooMin <soom9611@gmail.com>
2020-06-28 19:07:08 +0900

    create BOJ_2916

---
+++ Baekjoon/Math/BOJ_2916.cpp
@@ -0,0 +1,19 @@
+//
+//  BOJ_2916.cpp
+//  Algorithm
+//
+//  Created by 조수민 on 2020/06/28.
+//  Copyright © 2020 조수민. All rights reserved.
+//
+//  자와 각도기(https://www.acmicpc.net/problem/2916)
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
