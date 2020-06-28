f9208519a28057cf13b351f9ec987c14bb710cef
ChoSooMin <soom9611@gmail.com>
2020-06-28 11:14:06 +0900

    modify user.email

---
+++ Baekjoon/Math/BOJ_1670.cpp
@@ -0,0 +1,24 @@
+//
+//  BOJ_1670.cpp
+//  Algorithm
+//
+//  Created by 조수민 on 2020/06/23.
+//  Copyright © 2020 조수민. All rights reserved.
+//
+//  정상 회담 2(https://www.acmicpc.net/problem/1670)
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int main()
+{
+    cin >> N; // N 입력 받기
+    
+    
+    
+    return 0;
+}
