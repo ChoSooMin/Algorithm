f4a9aac78df5c4aada201a65075b2502fd50e0a8
ChoSooMin <soom9611@gmail.com>
2020-06-28 12:09:58 +0900

    [solved] BOJ_1670

--- Baekjoon/Math/BOJ_1670.cpp
+++ Baekjoon/Math/BOJ_1670.cpp
@@ -12,13 +12,29 @@
 
 using namespace std;
 
-int N;
+const long MOD = 987654321;
+long dp[10002];
+
+long getNumber(int N)
+{
+    dp[0] = dp[2] = 1; // 0명일 때와 2명일 때 악수하는 경우의 수는 1번
+    
+    for (int i = 4; i <= N; i+=2) // 2씩 증가해야 한다. (홀수면 악수하는 인원이 딱 나누어 떨어지지 않는다.)
+        for (int j = 0; j <= i - 2; j+= 2) // dp값 구하기
+        {
+            dp[i] += dp[j] * dp[i - j - 2];
+            dp[i] %= MOD;
+        }
+    
+    return dp[N];
+}
 
 int main()
 {
-    cin >> N; // N 입력 받기 
+    int N;
+    cin >> N; // N 입력 받기
     
-    
+    cout << getNumber(N) << endl; // 출력
     
     return 0;
 }
