f914dd35cf59cc1cef06863a75489147abffa4a1
ChoSooMin <soom9611@gmail.com>
2020-07-14 19:19:35 +0900

    [solved & problem] BOJ_1788

--- Baekjoon/Materialization/BOJ_1788.cpp
+++ Baekjoon/Materialization/BOJ_1788.cpp
@@ -11,38 +11,55 @@
 #include <stdio.h>
 
 #define MOD 1000000000
-#define ABS(X) ((X) > 0 ? (X) : (X) * -1)
 
 using namespace std;
 
-int arrp[1000001];
-int n;
+int fibonacci[1000001];
+
+int getFibonacci(int n)
+{
+    if (n == 0) // 0일 때
+        return fibonacci[0];
+    else if (n > 0) // 양수일 때
+        return fibonacci[n];
+    else // 음수일 때
+    {
+        if (n % 2 == 0) // 2로 나누어 떨어질 때 (절대값이 짝수일 때)
+            return fibonacci[n * (-1)] * (-1);
+        else // 2로 나누어 떨어지지 않을 때 (절대값이 홀수일 때)
+            return fibonacci[n * (-1)];
+    }
+}
 
 int main()
 {
+    int n;
+    
     // fibonacci 배열에 양수일 때의 피보나치 값을 구해서 넣어둔다.
-    arrp[0] = 0;
-    arrp[1] = 1;
+    fibonacci[0] = 0;
+    fibonacci[1] = 1;
     
     for (int i = 2; i <= 1000000; i++)
-        arrp[i] = (arrp[i - 1] + arrp[i - 2]) % MOD;
+        fibonacci[i] = (fibonacci[i - 1] + fibonacci[i - 2]) % MOD; // 1) 여기서 MOD로 나눴는데
     
     cin >> n; // n을 입력받는다.
     
     // 출력
-    if (n > 0)
+    if (n > 0) // n이 양수일 때
         cout << 1 << endl;
-    else if (n == 0)
+    else if (n == 0) // n이 0일 때
         cout << 0 << endl;
-    else
+    else // n이 음수일 때
     {
-        if (n % 2 == 0)
+        if (n % 2 == 0) // n이 2로 나누어 떨어지면 피보나치 값이 음수이다.
             cout << -1 << endl;
-        else
+        else // n이 2로 나누어 떨어지지 않으면 피보나치 값이 양수이다.
             cout << 1 << endl;
     }
-    cout << arrp[ABS(n)] % MOD << endl;
+    cout << fibonacci[abs(n)] % MOD << endl; // 2) 왜 여기서 MOD로 또 나누지..?
+    
         
     return 0;
 }
 
+// 이게 맞는 답인데 의문점 1 & 2
