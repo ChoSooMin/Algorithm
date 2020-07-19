8b5a9853413614f73a99373d8f2b87fd0c215f85
ChoSooMin <soom9611@gmail.com>
2020-07-19 11:30:55 +0900

    create BOJ_200

---
+++ Baekjoon/Materialization/BOJ_2002.cpp
@@ -0,0 +1,18 @@
+//
+//  BOJ_2002.cpp
+//  Algorithm
+//
+//  Created by 조수민 on 2020/07/19.
+//  Copyright © 2020 조수민. All rights reserved.
+//
+//  추월(https://www.acmicpc.net/problem/2002)
+
+#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+    
+    
+    return 0;
+}
