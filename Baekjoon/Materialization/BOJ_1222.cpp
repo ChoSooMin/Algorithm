2f57fc612f05272d320f70a2930368784564f83b
ChoSooMin <soom9611@gmail.com>
2020-07-23 12:55:25 +0900

    create BOJ_1222

---
+++ Baekjoon/Materialization/BOJ_1222.cpp
@@ -0,0 +1,18 @@
+//
+//  BOJ_1222.cpp
+//  Algorithm
+//
+//  Created by 조수민 on 2020/07/23.
+//  Copyright © 2020 조수민. All rights reserved.
+//
+//  홍준 프로그래밍 대회(https://www.acmicpc.net/problem/1222)
+
+#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+    
+    
+    return 0;
+}
