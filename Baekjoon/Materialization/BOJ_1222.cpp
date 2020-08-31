de9f7da2fb40782bfd1137dcea21018bc95b84b9
ChoSooMin <soom9611@gmail.com>
2020-08-31 19:08:14 +0900

    [timeout] BOJ_1222

--- Baekjoon/Materialization/BOJ_1222.cpp
+++ Baekjoon/Materialization/BOJ_1222.cpp
@@ -9,10 +9,57 @@
 
 #include <stdio.h>
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N; // 대회에 참가하는 학교의 수
+vector<int> studentNumber; // 각 학교의 학생 수 벡터
+
+void input()
+{
+    
+    cin >> N;
+    studentNumber.resize(N);
+    
+    for (int i = 0; i < N; i++)
+        cin >> studentNumber[i];
+}
+
+int getMaxNum()
+{
+    int max = 0;
+    
+    for (int i = 0; i < studentNumber.size(); i++)
+    {
+        int schoolNum = 0;
+        int currentNum = studentNumber[i];
+        
+        for (int j = 0; j < studentNumber.size(); j++)
+        {
+            if (studentNumber[j] % currentNum == 0)
+            {
+                schoolNum += 1;
+            }
+        }
+        
+        if (schoolNum == 1)
+            continue;
+        
+        int current = schoolNum * currentNum;
+        
+        if (max < current)
+            max = current;
+    }
+    
+    return max;
+}
 
 int main()
 {
+    input();
     
+    cout << getMaxNum() << endl;
     
     return 0;
 }
