53ee9912975e206d48a0c8e5d5d489ba8d57deda
ChoSooMin <soom9611@gmail.com>
2020-09-07 18:19:24 +0900

    remove BOJ_9372 & create BOJ_1058

--- Baekjoon/Graph/BOJ_1058.cpp
+++ Baekjoon/Graph/BOJ_1058.cpp
@@ -1,10 +1,11 @@
 //
-//  BOJ_9372.cpp
+//  BOJ_1058.cpp
 //  Algorithm
 //
 //  Created by 조수민 on 2020/09/07.
 //  Copyright © 2020 조수민. All rights reserved.
 //
+//  친구(https://www.acmicpc.net/problem/1058)
 
 #include <stdio.h>
 #include <iostream>
