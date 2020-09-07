9a82c5ecf9267812bd192fca67cd066f66eb4b6b
ChoSooMin <soom9611@gmail.com>
2020-09-07 17:11:20 +0900

    create BOJ_9372

---
+++ Baekjoon/BFS/BOJ_9372.cpp
@@ -0,0 +1,17 @@
+//
+//  BOJ_9372.cpp
+//  Algorithm
+//
+//  Created by 조수민 on 2020/09/07.
+//  Copyright © 2020 조수민. All rights reserved.
+//
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}
