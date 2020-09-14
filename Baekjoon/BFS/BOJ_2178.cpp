344907e71850e05c7e4a5215dd6304b64e46f4f1
ChoSooMin <soom9611@gmail.com>
2020-09-14 20:32:02 +0900

    create BOJ_2178

---
+++ Baekjoon/BFS/BOJ_2178.cpp
@@ -0,0 +1,20 @@
+//
+//  BOJ_2178.cpp
+//  Algorithm
+//
+//  Created by 조수민 on 2020/09/14.
+//  Copyright © 2020 조수민. All rights reserved.
+//
+//  미로 탐색(https://www.acmicpc.net/problem/2178)
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    
+    return 0;
+}
