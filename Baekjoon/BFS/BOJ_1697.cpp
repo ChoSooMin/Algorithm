96f62cdcf51402a2dd57ece8db94ac1f64555d55
ChoSooMin <soom9611@gmail.com>
2020-09-24 16:17:01 +0900

    create BOJ_1697

---
+++ Baekjoon/BFS/BOJ_1697.cpp
@@ -0,0 +1,21 @@
+//
+//  BOJ_1697.cpp
+//  Algorithm
+//
+//  Created by 조수민 on 2020/09/24.
+//  Copyright © 2020 조수민. All rights reserved.
+//
+//  숨바꼭질(https://www.acmicpc.net/problem/1697)
+
+#include <stdio.h>
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    
+    
+    return 0;
+}
