7de8471b449151f5e878669ccd287f4a0ed8d190
ChoSooMin <soom9611@gmail.com>
2020-09-25 03:56:35 +0900

    [unsolved] BOJ_1697

--- Baekjoon/BFS/BOJ_1697.cpp
+++ Baekjoon/BFS/BOJ_1697.cpp
@@ -11,11 +11,49 @@
 #include <iostream>
 #include <queue>
 
+#define MAX 100000
+
 using namespace std;
+
+int n, k;
+queue<int> Q;
+
+void input()
+{
+    cin >> n >> k;
+}
 
 int main()
 {
+    input();
     
+    Q.push(n);
+
+    int time = 0;
+    
+    while (!Q.empty())
+    {
+        auto cur = Q.front();
+        Q.pop();
+        
+        
+        if (cur == k)
+        {
+            cout << time;
+            return 0;
+        }
+        
+        else
+        {
+            if (cur + 1 > MAX || cur - 1 < 0 || 2 * cur > MAX)
+                continue;
+            
+            
+            Q.push(cur + 1);
+            Q.push(cur - 1);
+            Q.push(2 * cur);
+        }
+    }
     
     return 0;
 }
