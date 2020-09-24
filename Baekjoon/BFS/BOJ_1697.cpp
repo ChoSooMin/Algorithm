367e2440ec3e5ce70f4e568dc2c08867c731c627
ChoSooMin <soom9611@gmail.com>
2020-09-25 04:03:27 +0900

    [solved] BOJ_1697

--- Baekjoon/BFS/BOJ_1697.cpp
+++ Baekjoon/BFS/BOJ_1697.cpp
@@ -11,49 +11,51 @@
 #include <iostream>
 #include <queue>
 
-#define MAX 100000
-
 using namespace std;
 
 int n, k;
 queue<int> Q;
+int dist[100002]; // n번째에서 몇번만에 갈 수 있는지 저장하는 배열
 
 void input()
 {
     cin >> n >> k;
+    
+    fill(dist, dist + 100001, -1);
+    dist[n] = 0;
+}
+
+void getBFS()
+{
+    Q.push(n);
+    
+    while (!Q.empty())
+    {
+        int cur = Q.front();
+        Q.pop();
+        
+        
+        for (int next : { cur - 1, cur + 1, 2 * cur }) // 이렇게 for문을 돌리면 next에는 각각 cur - 1, cur + 1, 2 * cur이 차례대로 들어온다!
+        {
+            if (next < 0 || next > 100000)
+                continue;
+            
+            if (dist[next] != -1)
+                continue; // 이미 방문했던 곳
+            
+            dist[next] = dist[cur] + 1;
+            Q.push(next);
+        }
+    }
+    
+    cout << dist[k];
 }
 
 int main()
 {
     input();
     
-    Q.push(n);
-
-    int time = 0;
-    
-    while (!Q.empty())
-    {
-        auto cur = Q.front();
-        Q.pop();
-        
-        
-        if (cur == k)
-        {
-            cout << time;
-            return 0;
-        }
-        
-        else
-        {
-            if (cur + 1 > MAX || cur - 1 < 0 || 2 * cur > MAX)
-                continue;
-            
-            
-            Q.push(cur + 1);
-            Q.push(cur - 1);
-            Q.push(2 * cur);
-        }
-    }
+    getBFS();
     
     return 0;
 }
