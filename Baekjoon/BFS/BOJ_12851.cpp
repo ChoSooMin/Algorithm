ed09c570324755d73266d052ac538443df6b5de4
ChoSooMin <soom9611@gmail.com>
2020-09-28 20:50:01 +0900

    create BOJ_12851

---
+++ Baekjoon/BFS/BOJ_12851.cpp
@@ -0,0 +1,21 @@
+//
+//  BOJ_12851.cpp
+//  Algorithm
+//
+//  Created by 조수민 on 2020/09/28.
+//  Copyright © 2020 조수민. All rights reserved.
+//
+//  숨바꼭질 2(https://www.acmicpc.net/problem/12851)
+
+#include <stdio.h>
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    
+    
+    return 0;
+}
