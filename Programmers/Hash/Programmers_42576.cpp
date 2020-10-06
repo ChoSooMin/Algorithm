e76bd863c522417998a66fd56af1690f2a3adb7e
ChoSooMin <soom9611@gmail.com>
2020-10-06 21:30:55 +0900

    [solved] Programmers_42576

--- Programmers/Hash/Programmers_42576.cpp
+++ Programmers/Hash/Programmers_42576.cpp
@@ -14,10 +14,26 @@
 
 using namespace std;
 
+/**
+ 처음에는 find를 써서 completion의 원소가 participant에 없는 것을 찾으려고 했는데
+ 완주자 명단에 동명이인이 있을 경우 너무 복잡해지는 문제가 있다.
+ => sort를 써서 벡터를 정렬 후 비교하는 방식으로 해결
+ */
 string solution(vector<string> participant, vector<string> completion) {
     string answer = "";
     
+    // 벡터 정렬 후 비교
+    sort(participant.begin(), participant.end());
+    sort(completion.begin(), completion.end());
     
+    for (int i = 0; i < participant.size(); i++)
+    {
+        if (participant[i] != completion[i])
+        {
+            answer = participant[i];
+            break; // break를 꼭 넣어야 한다! 그렇지 않으면 이 뒤에도 순서가 계속 맞지 않기 때문에 결국 participant의 가장 마지막 원소를 answer에 대입하게 된다.
+        }
+    }
     
     return answer;
 }
@@ -27,17 +43,17 @@ int main()
     vector<string> participant1 = { "leo", "kiki", "eden" };
     vector<string> completion1 = { "eden", "kiki" };
     
-    cout << solution(participa1, completion1) << endl;
+    cout << solution(participant1, completion1) << endl;
     
     vector<string> participant2 = { "marina", "josipa", "nikola", "vinko", "filipa" };
     vector<string> completion2 = { "josipa", "filipa", "marina", "nikola" };
     
-    cout << solution(participa2, completion2) << endl;
+    cout << solution(participant2, completion2) << endl;
     
     vector<string> participant3 = { "mislav", "stanko", "mislav", "ana" };
-    vector<string> competion3 = { "stanko", "ana", "mislav" };
+    vector<string> completion3 = { "stanko", "ana", "mislav" };
     
-    cout << solution(participa3, completion3) << endl;
+    cout << solution(participant3, completion3) << endl;
     
     return 0;
 }
