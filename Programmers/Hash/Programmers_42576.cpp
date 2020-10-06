1fde89beb5e1004f8616360f9f454a4269eb341b
ChoSooMin <soom9611@gmail.com>
2020-10-06 21:05:47 +0900

    add example Programmers_42576

--- Programmers/Hash/Programmers_42576.cpp
+++ Programmers/Hash/Programmers_42576.cpp
@@ -16,10 +16,28 @@ using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {
     string answer = "";
+    
+    
+    
     return answer;
 }
 
 int main()
 {
+    vector<string> participant1 = { "leo", "kiki", "eden" };
+    vector<string> completion1 = { "eden", "kiki" };
+    
+    cout << solution(participa1, completion1) << endl;
+    
+    vector<string> participant2 = { "marina", "josipa", "nikola", "vinko", "filipa" };
+    vector<string> completion2 = { "josipa", "filipa", "marina", "nikola" };
+    
+    cout << solution(participa2, completion2) << endl;
+    
+    vector<string> participant3 = { "mislav", "stanko", "mislav", "ana" };
+    vector<string> competion3 = { "stanko", "ana", "mislav" };
+    
+    cout << solution(participa3, completion3) << endl;
+    
     return 0;
 }
