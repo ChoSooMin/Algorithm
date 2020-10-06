ce5fba0fac9bae63bab6d8652678c8fd7eebdeac
ChoSooMin <soom9611@gmail.com>
2020-10-06 21:01:26 +0900

    create Programmers_42576

---
+++ Programmers/Hash/Programmers_42576.cpp
@@ -0,0 +1,25 @@
+//
+//  Programmers_42576.cpp
+//  multi-target
+//
+//  Created by 조수민 on 2020/10/06.
+//  Copyright © 2020 조수민. All rights reserved.
+//
+//  완주하지 못한 선수(https://programmers.co.kr/learn/courses/30/lessons/42576)
+
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    return answer;
+}
+
+int main()
+{
+    return 0;
+}
