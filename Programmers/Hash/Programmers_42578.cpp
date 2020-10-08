2ec10c414ede0d86c769a01df0e6ebdd31b46e06
ChoSooMin <soom9611@gmail.com>
2020-10-08 21:54:25 +0900

    create Programmers_42578

---
+++ Programmers/Hash/Programmers_42578.cpp
@@ -0,0 +1,27 @@
+//
+//  Programmers_42578.cpp
+//  multi-target
+//
+//  Created by 조수민 on 2020/10/08.
+//  Copyright © 2020 조수민. All rights reserved.
+//
+//  위장(https://programmers.co.kr/learn/courses/30/lessons/42578)
+
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 0;
+    return answer;
+}
+
+int main()
+{
+    
+    
+    return 0;
+}
