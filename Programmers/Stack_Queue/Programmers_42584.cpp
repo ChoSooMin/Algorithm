8d34aa3585ac055647faaa79afed83671ff426aa
ChoSooMin <soom9611@gmail.com>
2020-10-09 17:18:25 +0900

    [solved] Programmers_42584

--- Programmers/Stack_Queue/Programmers_42584.cpp
+++ Programmers/Stack_Queue/Programmers_42584.cpp
@@ -8,6 +8,7 @@
 //  주식가격(https://programmers.co.kr/learn/courses/30/lessons/42584)
 
 #include <stdio.h>
+#include <iostream>
 #include <string>
 #include <vector>
 
@@ -15,16 +16,27 @@ using namespace std;
 
 vector<int> solution(vector<int> prices)
 {
-    vector<int> answer;
+    vector<int> answer(prices.size());
     
-    
-    
+    for (int i = 0; i < prices.size(); i++)
+        for (int j = i + 1; j < prices.size(); j++)
+        {
+            answer[i]++;
+            
+            if (prices[i] > prices[j])
+                break;
+        }
     return answer;
 }
 
 int main()
 {
+    vector<int> prices = { 1, 2, 3, 2, 3 };
     
+    vector<int> s = solution(prices);
+    
+    for (int i : s)
+        cout << i << " " << endl;
     
     return 0;
 }
