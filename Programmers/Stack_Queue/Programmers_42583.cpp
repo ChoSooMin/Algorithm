3cd9352c856a1fa454e5a549f1a5470d585fcdbe
ChoSooMin <soom9611@gmail.com>
2020-10-10 02:57:33 +0900

    create Programmers_42583

---
+++ Programmers/Stack_Queue/Programmers_42583.cpp
@@ -0,0 +1,28 @@
+//
+//  Programmers_42583.cpp
+//  Algorithm
+//
+//  Created by 조수민 on 2020/10/10.
+//  Copyright © 2020 조수민. All rights reserved.
+//
+//  다리를 지나가는 트럭(https://programmers.co.kr/learn/courses/30/lessons/42583)
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int bridge_length, int weight, vector<int> truck_weights)
+{
+    int answer = 0;
+    return answer;
+}
+
+int main()
+{
+    
+    
+    return 0;
+}
