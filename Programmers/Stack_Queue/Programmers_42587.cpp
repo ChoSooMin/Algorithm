b9d9b82f8dec27c2ade1582ad996a179a47c65d2
ChoSooMin <soom9611@gmail.com>
2020-10-14 17:49:05 +0900

    create Programmers_42587

---
+++ Programmers/Stack_Queue/Programmers_42587.cpp
@@ -0,0 +1,28 @@
+//
+//  Programmers_42587.cpp
+//  Algorithm
+//
+//  Created by 조수민 on 2020/10/14.
+//  Copyright © 2020 조수민. All rights reserved.
+//
+//  프린터(https://programmers.co.kr/learn/courses/30/lessons/42587)
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> priorities, int location)
+{
+    int answer = 0;
+    
+    
+    return answer;
+}
+
+int main()
+{
+    return 0;
+}
