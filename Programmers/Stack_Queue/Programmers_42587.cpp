14e9a95b4d342145912360357f23e05ea8062c5d
ChoSooMin <soom9611@gmail.com>
2020-10-15 23:15:02 +0900

    add explanation in Programmers_42587

--- Programmers/Stack_Queue/Programmers_42587.cpp
+++ Programmers/Stack_Queue/Programmers_42587.cpp
@@ -10,7 +10,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
-#include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -18,11 +18,30 @@ int solution(vector<int> priorities, int location)
 {
     int answer = 0;
     
+    /**
+     priorityQ에는 priorities의 값이 들어가고,
+     indexQ에는 해당 인덱스들이 들어간다.
+     */
+    queue<int> priorityQ;
+    queue<int> indexQ;
+    
+    while(true)
+    {
+        
+    }
     
     return answer;
 }
 
 int main()
 {
+    vector<int> priorities1 = { 2, 1, 3, 2 };
+    int location1 = 2;
+    
+    vector<int> priorities2 = { 1, 1, 9, 1, 1, 1 };
+    int location2 = 0;
+    
+    cout << solution(priorities1, location1) << solution(priorities2, location2) << endl;
+    
     return 0;
 }
