c004ca613c7dc128ee677531ba5e324b2e6e19e9
ChoSooMin <soom9611@gmail.com>
2020-10-16 17:10:30 +0900

    [solved] Programmers_42587

--- Programmers/Stack_Queue/Programmers_42587.cpp
+++ Programmers/Stack_Queue/Programmers_42587.cpp
@@ -19,15 +19,35 @@ int solution(vector<int> priorities, int location)
     int answer = 0;
     
     /**
-     priorityQ에는 priorities의 값이 들어가고,
-     indexQ에는 해당 인덱스들이 들어간다.
+     queue<pair<int, int>>를 만들어 (인덱스, 우선순위값) 짝을 갖는 큐를 만든다.
      */
-    queue<int> priorityQ;
-    queue<int> indexQ;
+    queue<pair<int, int>> pairQ;
+    priority_queue<int> priorityQ;
     
-    while(true)
+    for (int i = 0; i < priorities.size(); i++)
     {
+        pairQ.push(make_pair(i, priorities[i]));
+        priorityQ.push(priorities[i]); // 이 큐에는 우선순위가 순서대로 있을 것ㅅ
+    }
+    
+    while (!pairQ.empty())
+    {
+        int currentIndex = pairQ.front().first;
+        int currentPriority = pairQ.front().second;
+        pairQ.pop();
         
+        // 현재 문서의 중요도가 가장 높은 중요도라면 인쇄
+        if (currentPriority == priorityQ.top())
+        {
+            priorityQ.pop();
+            answer += 1;
+            
+            // 현재 문서가 내가 인쇄를 요청한 문서이면 멈춘다
+            if (currentIndex == location)
+                break;
+        }
+        else // 아니라면 큐에 다시 넣는다
+            pairQ.push(make_pair(currentIndex, currentPriority));
     }
     
     return answer;
