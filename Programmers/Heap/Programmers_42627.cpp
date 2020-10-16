d27947e8179034d3c3f50e836d73ea6a5943a83c
ChoSooMin <soom9611@gmail.com>
2020-10-16 17:40:31 +0900

    create Programmers_42627

---
+++ Programmers/Heap/Programmers_42627.cpp
@@ -0,0 +1,21 @@
+//
+//  Programmers_42627.cpp
+//  Algorithm
+//
+//  Created by 조수민 on 2020/10/16.
+//  Copyright © 2020 조수민. All rights reserved.
+//
+//  디스크 컨트롤러(https://programmers.co.kr/learn/courses/30/lessons/42627)
+
+#include <stdio.h>
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int soluton(vector<vector<int>> jobs)
+{
+    int answer = 0;
+    
+    return answer;
+}
