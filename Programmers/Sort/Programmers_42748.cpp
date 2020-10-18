5e0ef525810ba6729ee550509057cf787061e25d
ChoSooMin <soom9611@gmail.com>
2020-10-18 20:40:56 +0900

    create Programmers_42748

---
+++ Programmers/Sort/Programmers_42748.cpp
@@ -0,0 +1,26 @@
+//
+//  Programmers_42748.cpp
+//  Algorithm
+//
+//  Created by 조수민 on 2020/10/18.
+//  Copyright © 2020 조수민. All rights reserved.
+//
+//  K번째 수(https://programmers.co.kr/learn/courses/30/lessons/42748)
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    return answer;
+}
+
+int main() {
+    
+    
+    return 0;
+}
