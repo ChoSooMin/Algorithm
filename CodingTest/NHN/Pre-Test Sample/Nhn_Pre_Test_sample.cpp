d08fcf0c33548fd4c822e9a0ada8aa463c181c47
ChoSooMin <soom9611@gmail.com>
2020-10-19 21:17:35 +0900

    [fail] Nhn_Pre_Test_sample

--- CodingTest/NHN/Pre-Test Sample/Nhn_Pre_Test_sample.cpp
+++ CodingTest/NHN/Pre-Test Sample/Nhn_Pre_Test_sample.cpp
@@ -18,7 +18,7 @@ vector<vector<int>> matrix;
 int dx[4] = { 1, 0, -1, 0};
 int dy[4] = { 0, 1, 0, -1 };
 vector<vector<int>> checkMatrix;
-queue<int> areaQ;
+priority_queue<int, vector<int>, greater<int>> areaQ;
 
 /**
  BFS 사용.
@@ -48,39 +48,64 @@ void input() {
 
 void BFS() {
     queue<pair<int, int>> Q;
-    
-    Q.push(make_pair(0, 0));
-    checkMatrix.at(0).at(0) = -1; // 지나온 곳을 체크해주기 위해 checkMatrix의 값을 -1로 바꿔준다.
     int areaSize = 0;
-    
-    while (true) {
-        pair<int, int> cur = Q.front(); Q.pop(); // 현재 위치 (cur.first, cur.second)
-        
-        for (int i = 0; i < 4; i++) {
-            // 근접한 위치 = (nextX, nextY)
-            int nextX = cur.first + dx[i];
-            int nextY = cur.second + dy[i];
-            
-            if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= N || checkMatrix.at(nextX).at(nextY) == -1) { // 범위를 벗어났을 경우 & 이미 들른 곳이라면
-                continue; // 건너뛴다.
+
+    for (int i = 0; i < matrix.size(); i++) {
+        for (int j = 0; j < matrix.at(i).size(); j++) {
+            areaSize = 0;
+            // 현재 위치가 0이거나 이미 접근한 곳이라면 넘어간다.
+            if (matrix.at(i).at(j) == 0 || checkMatrix.at(i).at(j) == -1) {
+                continue;
             }
             
-            if (matrix.at(nextX).at(nextY) == 1) { // 영역이라면
-                areaSize++;
-                Q.push(make_pair(nextX, nextY)); // 큐에 넣는다.
-            }
-            else if (matrix.at(nextX).at(nextY) == 0) {
+            // 이미 접근하지 않았고, 영역에 속할 때 (1일 때)
+            Q.push(make_pair(i, j));
+            checkMatrix.at(i).at(j) == -1; // 방문했다는 표시를 남긴다.
+            
+            while (!Q.empty()) {
+                auto current = Q.front(); Q.pop(); // 큐의 제일 앞에 있는 값을 가져온다.
+                areaSize++; // 큐에서 원소를 하나씩 뺄때마다 영역크기 +1 해준다.
                 
+                for (int dir = 0; dir < 4; dir++) {
+                    // 근접한 곳(다음)
+                    int nx = current.first + dx[dir];
+                    int ny = current.second + dy[dir];
+                    
+                    if (nx < 0 || nx >= matrix.size() || ny < 0 || ny >= matrix.at(i).size()) { // 범위 밖일 경우 넘어간다.
+                        continue;
+                    }
+                    if (checkMatrix.at(nx).at(ny) == -1 || matrix.at(nx).at(ny) != 1) { // 이미 방문한 곳이거나 영역에 속하지 않으면 넘어간다.
+                        continue;
+                    }
+                    
+                    checkMatrix.at(nx).at(ny) = -1; // 방문했다고 체크해준다.
+                    Q.push(make_pair(nx, ny)); // 큐에 넣어준다.
+                }
             }
             
-            checkMatrix.at(nextX).at(nextY) = -1; // 들렀다고 표시
+            // (i, j)를 시작점으로 하는 BFS가 종료
+            areaQ.push(areaSize); // areaSize를 영역 큐에 넣어준다.
         }
+    }
+    
+    int sizeCount = areaQ.size();
+    if (sizeCount == 0) {
+        cout << sizeCount << endl;
+        return;
+    }
+    else {
+        cout << sizeCount << endl;
+        while (!areaQ.empty()) {
+            auto cur = areaQ.top(); areaQ.pop();
+            cout << cur << " " ;
+        }
+        cout << endl;
     }
 }
 
 int main() {
     input();
-    
+    BFS();
     
     return 0;
 }
