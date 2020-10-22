d46351673b4ba045b75f145449eb079156693f35
ChoSooMin <soom9611@gmail.com>
2020-10-22 17:39:32 +0900

    create BOJ_10451

---
+++ Baekjoon/BOJ_10451.cpp
@@ -0,0 +1,18 @@
+//
+//  BOJ_10451.cpp
+//  Algorithm
+//
+//  Created by 조수민 on 2020/10/22.
+//  Copyright © 2020 조수민. All rights reserved.
+//
+//  순열 사이클(https://www.acmicpc.net/problem/10451)
+
+#include <stdio.h>
+#include <iostream>
+
+
+
+int main() {
+    
+    return 0;
+}
