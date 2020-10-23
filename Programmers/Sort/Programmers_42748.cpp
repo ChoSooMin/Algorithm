70264e2d926662d5926b334f4d052f5c77dec651
ChoSooMin <soom9611@gmail.com>
2020-10-23 16:41:27 +0900

    [solved] Programmers_42748

--- Programmers/Sort/Programmers_42748.cpp
+++ Programmers/Sort/Programmers_42748.cpp
@@ -11,19 +11,52 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
+
+int getNum(vector<int> array, vector<int> command) {
+    int answer = -1;
+    
+    // i, j, k 구하기
+    int i = command.at(0);
+    int j = command.at(1);
+    int k = command.at(2);
+    int indexI = i - 1;
+    int indexJ = j - 1;
+    
+    // 새로운 벡터에 넣는다.
+    vector<int> cuttingVector;
+    for (int a = indexI; a <= indexJ; a++) {
+        cuttingVector.push_back(array.at(a));
+    }
+    
+    sort(cuttingVector.begin(), cuttingVector.end());
+    
+    answer = cuttingVector.at(k - 1);
+    
+    return answer;
+}
 
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
     
-    
+    for (int i = 0; i < commands.size(); i++) {
+        int getAns = getNum(array, commands.at(i));
+        answer.push_back(getAns);
+    }
     
     return answer;
 }
 
 int main() {
-    
+    vector<int> array = { 1, 5, 2, 6, 3, 7, 4 };
+    vector<vector<int>> commands = { { 2, 5, 3 }, { 4, 4, 1 }, { 1, 7, 3 } };
+    vector<int> sol = solution(array, commands);
+    for (int i = 0; i < sol.size(); i++) {
+        cout << sol.at(i) << " ";
+    }
+    cout << endl;
     
     return 0;
 }
