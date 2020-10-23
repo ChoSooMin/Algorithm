bf1753a4518f58948c5f3601e085798908e3157f
ChoSooMin <soom9611@gmail.com>
2020-10-23 23:24:17 +0900

    create Programmers_42747

---
+++ Programmers/Sort/Programmers_42747.cpp
@@ -0,0 +1,28 @@
+//
+//  Programmers_42747.cpp
+//  Algorithm
+//
+//  Created by 조수민 on 2020/10/23.
+//  Copyright © 2020 조수민. All rights reserved.
+//
+//  H-index(https://programmers.co.kr/learn/courses/30/lessons/42747)
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    int answer = 0;
+    
+    
+    return answer;
+}
+
+int main() {
+    
+    
+    return 0;
+}
