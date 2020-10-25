b484e1eb0108da3e8319fb8de3e24db1aefe8fbe
ChoSooMin <soom9611@gmail.com>
2020-10-25 22:39:16 +0900

    [solved] Programmers_42747

--- Programmers/Sort/Programmers_42747.cpp
+++ Programmers/Sort/Programmers_42747.cpp
@@ -26,21 +26,10 @@ int solution(vector<int> citations) {
     sort(citations.begin(), citations.end(), greater<>()); // 벡터 내림차순 정렬을 greater<>() 사용
     for (int i = 0; i < citations.size(); i++) {
         int curNum = citations.at(i);
-        /**
-         만약 curNum이 6일 경우, 절대 H-index가 될 수 없다.
-         6번 이상 인용된 논문이 6편 이상이어야 하는데, 논문 수는 5개 이므로
-         */
-        if (curNum > citations.size()) {
-            continue;
-        }
         
-        int upperCount = i + 1; // h편 이상 인용된 논문의 개수
-        int lowerCount = citations.size() - upperCount; // h편 미만 인용된 논문의 개수
-        
-        if (upperCount >= curNum && lowerCount < curNum) {
-            answer = curNum;
+        if (citations.at(i) <= answer)
             break;
-        }
+        answer++;
     }
     
     return answer;
