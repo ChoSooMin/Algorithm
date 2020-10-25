82781890bc9ffc6bdb9832642cd2aae1be353fa7
ChoSooMin <soom9611@gmail.com>
2020-10-25 17:31:49 +0900

    create BOJ_14719

---
+++ Baekjoon/BOJ_14719.cpp
@@ -0,0 +1,18 @@
+//
+//  BOJ_14719.cpp
+//  Algorithm
+//
+//  Created by 조수민 on 2020/10/25.
+//  Copyright © 2020 조수민. All rights reserved.
+//
+//  빗물(https://www.acmicpc.net/problem/14719)
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
