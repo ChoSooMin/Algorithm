68b289d90eb3a35bef5a008dc534907c1752dc1d
ChoSooMin <soom9611@gmail.com>
2020-10-25 22:23:17 +0900

    [fail] Programmers_42747

--- Programmers/Sort/Programmers_42747.cpp
+++ Programmers/Sort/Programmers_42747.cpp
@@ -11,6 +11,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -23,17 +24,30 @@ int solution(vector<int> citations) {
      6, 5, 3, 1, 0
      */
     sort(citations.begin(), citations.end(), greater<>()); // 벡터 내림차순 정렬을 greater<>() 사용
-    int upperCount = 0;
-    int lowerCount = 0;
     for (int i = 0; i < citations.size(); i++) {
         int curNum = citations.at(i);
+        /**
+         만약 curNum이 6일 경우, 절대 H-index가 될 수 없다.
+         6번 이상 인용된 논문이 6편 이상이어야 하는데, 논문 수는 5개 이므로
+         */
+        if (curNum > citations.size()) {
+            continue;
+        }
+        
+        int upperCount = i + 1; // h편 이상 인용된 논문의 개수
+        int lowerCount = citations.size() - upperCount; // h편 미만 인용된 논문의 개수
+        
+        if (upperCount >= curNum && lowerCount < curNum) {
+            answer = curNum;
+            break;
+        }
     }
     
     return answer;
 }
 
 int main() {
-    solution({ 3, 0, 6, 1, 5 });
+    cout << solution({ 3, 0, 6, 1, 5 });
     
     return 0;
 }
