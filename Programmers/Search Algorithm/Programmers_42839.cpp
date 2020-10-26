9df7b8dd040b6a9a4f2fb3b288337b11a4751749
ChoSooMin <soom9611@gmail.com>
2020-10-26 23:44:12 +0900

    create Programmers_42839

---
+++ Programmers/Search Algorithm/Programmers_42839.cpp
@@ -0,0 +1,27 @@
+//
+//  Programmers_42839.cpp
+//  Algorithm
+//
+//  Created by 조수민 on 2020/10/26.
+//  Copyright © 2020 조수민. All rights reserved.
+//
+//  소수 찾기(https://programmers.co.kr/learn/courses/30/lessons/42839)
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string numbers) {
+    int answer = 0;
+    
+    return answer;
+}
+
+int main() {
+    
+    
+    return 0;
+}
