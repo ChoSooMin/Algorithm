e58e97678c7d0cd1571d3f6f53147fb085013073
ChoSooMin <soom9611@gmail.com>
2020-10-26 20:34:47 +0900

    create Programmers_42840

---
+++ Programmers/Search Algorithm/Programmers_42840.cpp
@@ -0,0 +1,28 @@
+//
+//  Programmers_42840.cpp
+//  Algorithm
+//
+//  Created by 조수민 on 2020/10/26.
+//  Copyright © 2020 조수민. All rights reserved.
+//
+//  모의고사(https://programmers.co.kr/learn/courses/30/lessons/42840)
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> answers) {
+    vector<int> answer;
+    
+    
+    return answer;
+}
+
+int main() {
+    
+    
+    return 0;
+}
