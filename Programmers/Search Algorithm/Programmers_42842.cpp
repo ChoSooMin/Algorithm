5d4e790e1b5d20261e08161200f8f1ec4dd7a73f
ChoSooMin <soom9611@gmail.com>
2020-10-28 20:58:56 +0900

    create Programmers_42842

---
+++ Programmers/Search Algorithm/Programmers_42842.cpp
@@ -0,0 +1,26 @@
+//
+//  Programmers_42842.cpp
+//  Algorithm
+//
+//  Created by 조수민 on 2020/10/28.
+//  Copyright © 2020 조수민. All rights reserved.
+//
+//  카펫(https://programmers.co.kr/learn/courses/30/lessons/42842)
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int brown, int yellow) {
+    vector<int> answer;
+    return answer;
+}
+
+int main() {
+    
+    
+    return 0;
+}
