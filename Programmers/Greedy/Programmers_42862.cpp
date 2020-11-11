e22b28b34b59ac093ec35045416e4b56c31d74e8
ChoSooMin <soom9611@gmail.com>
2020-11-11 23:53:47 +0900

    create Programmers_42862

---
+++ Programmers/Greedy/Programmers_42862.cpp
@@ -0,0 +1,26 @@
+//
+//  Programmers_42862.cpp
+//  Algorithm
+//
+//  Created by 조수민 on 2020/11/11.
+//  Copyright © 2020 조수민. All rights reserved.
+//
+//  체육복(https://programmers.co.kr/learn/courses/30/lessons/42862)
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, vector<int> lost, vector<int> reserve) {
+    int answer = 0;
+    return answer;
+}
+
+int main() {
+    
+    
+    return 0;
+}
