b05a534509c4a9873e4c21b0296f7f6f27555741
ChoSooMin <soom9611@gmail.com>
2020-11-12 14:59:35 +0900

    create Programmers_42883

---
+++ Programmers/Greedy/Programmers_42883.cpp
@@ -0,0 +1,29 @@
+//
+//  Programmers_42883.cpp
+//  Algorithm
+//
+//  Created by 조수민 on 2020/11/12.
+//  Copyright © 2020 조수민. All rights reserved.
+//
+//  큰 수 만들기(https://programmers.co.kr/learn/courses/30/lessons/42883)
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string number, int k) {
+    string answer = "";
+    
+    
+    
+    return answer;
+}
+
+int main() {
+    
+    
+    return 0;
+}
