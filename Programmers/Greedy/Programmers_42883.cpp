413da662b6a8b7b57e970dba7e9c46320c52c46e
ChoSooMin <soom9611@gmail.com>
2020-11-13 17:26:32 +0900

    [unsolved] solution Programmers_42883

--- Programmers/Greedy/Programmers_42883.cpp
+++ Programmers/Greedy/Programmers_42883.cpp
@@ -10,81 +10,41 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
-#include <vector>
 
 using namespace std;
 
 string solution(string number, int k) {
     string answer = "";
     
-    int remainLength = number.length() - k;
-    int max = -1;
-    int maxIndex = -1;
+    int start = 0, maxIndex = 0;
+    char maxNum = NULL;
     
-    for (int i = 0; i < number.size() - k + 1; i++) {
-        char cur = number.at(i);
-        int curToInt = cur - '0'; // char를 int형으로 변환
+    /**
+     num : 4 1 7 7 2 5 || 2 8 4 1
+     ind : 0 1 2 3 4 5 || 6 7 8 9
+     ind1: 1 2 3 4 5 6 || 7 8 9 10
+     number.size() - k = 6 (6개 선택할 수 있다.)
+     */
+    for (int i = 0; i < number.size() - k; i++) {
+        maxNum = number[start];
+        maxIndex = start;
         
-        if (max < curToInt) {
-            max = curToInt;
-            maxIndex = i;
-        }
-    }
-    answer += to_string(max);
-    
-    k -= maxIndex;
-    cout << "max : " << max << " maxIndex : " << maxIndex << " remain k : " << k;
-    cout << endl;
-    
-    remainLength -= 1;
-    
-    if (k == 1) { // k가 1만 남았을 때는 뒤의 원소들 중 가장 작은 애만 빼면 된다,,?
-        int min = 10;
-        int minIndex = -1;
-        for (int i = maxIndex + 1; i < number.size(); i++) {
-            char cur = number.at(i);
-            int curToInt = cur - '0';
-            
-            if (min > curToInt) {
-                min = curToInt;
-                minIndex = i;
+        for (int j = start; j <= i + k; j++) {
+            if (maxNum < number[j]) {
+                maxNum = number[j];
+                maxIndex = j;
             }
         }
         
-        for (int i = maxIndex + 1; i < number.size(); i++) {
-            if (i == minIndex)
-                continue;
-            answer += number.at(i);
-        }
-    }
-    else {
-        int secondMax = -1;
-        int secondMaxIndex = -1;
-        
-        for (int i = maxIndex + 1; i < number.length() - remainLength; i++) {
-            
-            char cur = number.at(i);
-            int curToInt = cur - '0';
-            
-            cout << "index : " << i << "cur : " << cur << endl;
-            
-            if (secondMax < curToInt) {
-                secondMax = curToInt;
-                secondMaxIndex = i;
-            }
-        }
-        
-        answer += to_string(secondMax);
-        k -= 1;
-        
-        cout << "secondMax < " << secondMax << " secondMaxIndex : " << secondMaxIndex << endl;
+        start = maxIndex + 1;
+        answer += maxNum;
     }
     
     return answer;
 }
 
 int main() {
-    solution("4177252841", 4);
+    cout << solution("4177252841", 4) << endl;
     
     return 0;
 }
