a3bb2d3f61f86a31cbf2ec3237e1ad24824af0f3
ChoSooMin <soom9611@gmail.com>
2020-11-18 23:14:31 +0900

    create BOJ_2667

---
+++ Baekjoon/BFS/BOJ_2667.cpp
@@ -0,0 +1,25 @@
+//
+//  BOJ_2667.cpp
+//  Algorithm
+//
+//  Created by 조수민 on 2020/11/18.
+//  Copyright © 2020 조수민. All rights reserved.
+//
+//  단지번호붙이기(https://www.acmicpc.net/problem/2667)
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+void getHouseCount() {
+    
+}
+
+int main() {
+    cin >> N;
+    
+    return 0;
+}
---
+++ Programmers/Greedy/Programmers_42860.cpp
@@ -0,0 +1,32 @@
+//
+//  Programmers_42860.cpp
+//  Algorithm
+//
+//  Created by 조수민 on 2020/11/13.
+//  Copyright © 2020 조수민. All rights reserved.
+//
+//  조이스틱(https://programmers.co.kr/learn/courses/30/lessons/42860)
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string name) {
+    int answer = 0;
+    
+    
+    for (int i = 0; i < name.length(); i++) {
+        
+    }
+    
+    return answer;
+}
+
+int main() {
+    
+    
+    return 0;
+}
