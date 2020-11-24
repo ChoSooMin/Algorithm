a36907141f8235b89cac384baf0c22c13efa70f4
ChoSooMin <soom9611@gmail.com>
2020-11-24 17:12:16 +0900

    [solved] Programmers_42860

--- Programmers/Greedy/Programmers_42860.cpp
+++ Programmers/Greedy/Programmers_42860.cpp
@@ -17,16 +17,42 @@ using namespace std;
 int solution(string name) {
     int answer = 0;
     
-    
-    for (int i = 0; i < name.length(); i++) {
+    int i = 0;
+    string temp(name.length(), 'A'); // temp = AAA
+    while (true) {
+        temp[i] = name[i];
         
+        // A쪽이 더 가까운지 Z쪽이 더 가까운지 비교하여 더 가까운 쪽을 answer에 더한다.
+        name[i] - 'A' > 'Z' + 1 - name[i] ? answer += 'Z' + 1 - name[i] : answer += name[i] - 'A';
+        
+        if (temp == name) // name이 만들어졌으면 break
+            break;
+        
+        // 왼쪽 or 오른쪽 둘 중 어디로 이동?
+        for (int move = 1; move < name.length(); move++) {
+            // 오른쪽 이동이 빠를 경우 오른쪽으로 이동하고 이동횟수 반영
+            if (name[(i + move) % name.length()] != temp[(i + move) % name.length()]) {
+                i = (i + move) % name.length();
+                answer += move;
+                
+                break;
+            }
+            // 왼쪽 이동이 빠를 경우 왼쪽으로 이동하고 이동횟수 반영
+            else if (name[(i + name.length() - move) % name.length()]
+                     != temp[(i + name.length() - move) % name.length()]) {
+                i = (i + name.length() - move) % name.length();
+                answer += move;
+                
+                break;
+            }
+        }
     }
     
     return answer;
 }
 
 int main() {
-    
+    cout << solution("JEROEN") << endl;
     
     return 0;
 }
