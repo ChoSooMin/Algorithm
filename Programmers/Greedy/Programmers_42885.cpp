316aa498c0e0b40d0ea59f3723661b7ef1ff62ca
ChoSooMin <soom9611@gmail.com>
2020-11-30 18:30:54 +0900

    [solved] Programmers_42885

--- Programmers/Greedy/Programmers_42885.cpp
+++ Programmers/Greedy/Programmers_42885.cpp
@@ -11,61 +11,37 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int p[50001];
-
 int solution(vector<int> people, int limit) {
-    int answer = 0;
+    int answer = 0; // 보트가 나간 횟수
+    int cnt = 0; // 나간 사람의 수
+    int begin = 0; // 뒤 인덱스
+    int end = 1; // 앞 인덱스
     
-    // limit에서 본인의 무게를 빼고 (1) 그 값보다 작거나 같은 애를 찾으면?
-    // 시간복잡도 괜찮나?
-    for (int i = 0; i < people.size(); i++) {
-        p[i] = people.at(i);
-    }
+    // 정렬
+    sort(people.begin(), people.end());
     
-    for (int i = 0; i < people.size(); i++) {
-        int cur = p[i];
+    while(true) {
+        // 제일 뒤의 무게를 넣기
+        int first = people.at(people.size() - end++);
+        cnt++; // 한 명 보트에 태움
         
-        int max = 0;
-        int maxIndex = -1;
-        
-        if (cur == 0) // 현재 위치의 사람이 이미 구조된 후라면 다음으로 넘긴다.
-            continue;
-        
-        int rest = limit - cur;
-        
-        if (i != people.size() - 1) {
-            for (int j = i + 1; j < people.size(); j++) {
-                int next = p[j];
-                
-                if (next == 0) // 마찬가지로 구조된 후라면 다음으로
-                    continue;
-                if (next > rest) // 남은 무게 제한보다 몸무게가 많이 나가면 다음으로
-                    continue;
-                
-                // 구조 X && 남은 무게 제한에 충족
-                if (max < next) {
-                    max = next;
-                    maxIndex = j;
-                }
-            }
-            
-            if (max != 0 && maxIndex != -1) { // 맞는 짝을 찾았을 때
-                answer++;
-                
-                p[i] = 0;
-                p[maxIndex] = 0;
-            }
+        // 최대 제한에서 제일 무거운 사람을 뻈을 때 나머지 자리에 가장 가벼운 사람이 탈 수 있다면
+        if (limit - first >= people.at(begin)) {
+            // 앞사람 인덱스 증가 & 보트에 탄 사람 추가
+            begin++;
+            cnt++;
         }
-    }
-    
-    for (int i = 0; i < people.size(); i++) {
-        if (p[i] == 0)
-            continue;
         
+        // 보트 나간 횟수 증가
         answer++;
+        
+        // 보트를 타고 나간 사람이 총인원보다 같거나 많으면 반복문 종료
+        if (cnt >= people.size())
+            break;
     }
     
     return answer;
