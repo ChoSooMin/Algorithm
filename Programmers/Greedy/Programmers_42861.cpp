4922cf4fd2c6b15d854340ed064de29bf401e669
ChoSooMin <soom9611@gmail.com>
2020-12-01 17:39:10 +0900

    [unsolved] Programmers_42861

---
+++ Programmers/Greedy/Programmers_42861.cpp
@@ -0,0 +1,33 @@
+//
+//  Programmers_42861.cpp
+//  Algorithm
+//
+//  Created by 조수민 on 2020/11/30.
+//  Copyright © 2020 조수민. All rights reserved.
+//
+//  섬 연결하기(https://programmers.co.kr/learn/courses/30/lessons/42861)
+
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, vector<vector<int>> costs) {
+    int answer = 0;
+    
+    vector<vector<int>> sortedCosts;
+    
+    // cost만 저장해두고,, 작은 cost부터 추가? (sort 사용)
+    for (int i = 0; i < costs.size(); i++) {
+        
+    }
+    
+    return answer;
+}
+
+int main() {
+    
+    
+    return 0;
+}
