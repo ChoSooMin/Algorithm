914cba097e80c80e0dcd3883486afbc3cce99b83
ChoSooMin <soom9611@gmail.com>
2020-12-01 17:54:02 +0900

    [solved] remove sort numbers in Programmers_68644

--- Programmers/Programmers_68644.cpp
+++ Programmers/Programmers_68644.cpp
@@ -18,8 +18,6 @@ using namespace std;
 vector<int> solution(vector<int> numbers) {
     vector<int> answer;
     
-    sort(numbers.begin(), numbers.end());
-    
     for (int i = 0; i < numbers.size() - 1; i++) {
         for (int j = i + 1; j < numbers.size(); j++) {
             int a = numbers.at(i);
