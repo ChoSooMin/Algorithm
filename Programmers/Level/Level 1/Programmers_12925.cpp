8bbc93d87e2d81a61423e4aa46622b0325ed6444
ChoSooMin <soom9611@gmail.com>
2021-01-01 23:36:20 +0900

    [solved] Programmers_12925

---
+++ Programmers/Level/Level 1/Programmers_12925.cpp
@@ -0,0 +1,27 @@
+//
+//  Programmers_12925.cpp
+//  Algorithm
+//
+//  Created by 조수민 on 2021/01/01.
+//  Copyright © 2021 조수민. All rights reserved.
+//
+//  문자열을 정수로 바꾸기(https://programmers.co.kr/learn/courses/30/lessons/12925)
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = stoi(s);
+    
+    return answer;
+}
+
+int main() {
+    cout << solution("-1234") << endl;
+    
+    return 0;
+}
