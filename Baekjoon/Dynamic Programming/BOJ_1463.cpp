f415f6668d7894bf9e78461036ebb651f5c49dd9
ChoSooMin <soom9611@gmail.com>
2021-01-08 01:38:52 +0900

    [solved] BOJ_1463

--- Baekjoon/Dynamic Programming/BOJ_1463.cpp
+++ Baekjoon/Dynamic Programming/BOJ_1463.cpp
@@ -15,39 +15,17 @@ using namespace std;
 
 int dp[1000001]; // dp 사용
 
-void getDp() {
-    dp[0] = 0;
-    dp[1] = 0;
-    dp[2] = 1;
-    
-    for (int i = 3; i < 1000001; i++) {
-        // 1. /3
-        int first = 1000;
-        int second = 1000;
-        int third = 1000;
-        if (i % 3 == 0) {
-            first = 1 + dp[i / 3];
-        }
-        
-        // 2. /2
-        if (i % 2 == 0) {
-            second = 1 + dp[i / 2];
-        }
-        
-        // 3. -1
-        third = 1 + dp[i - 1];
-        
-        int min1 = min(first, second);
-        int min2 = min(min1, third);
-        
-        dp[i] = min2;
-    }
-}
-
 int main() {
     int N;
     cin >> N;
-    getDp();
+    
+    for (int i = 2; i <= N; i++) {
+        dp[i] = dp[i - 1] + 1;
+        if (i % 2 == 0)
+            dp[i] = min(dp[i], dp[i / 2] + 1);
+        if (i % 3 == 0)
+            dp[i] = min(dp[i], dp[i / 3] + 1);
+    }
     
     cout << dp[N] << endl;
     
