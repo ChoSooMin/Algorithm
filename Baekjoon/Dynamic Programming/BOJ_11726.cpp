2f48de2481e481af2e56ac52920b41e2ab4486bb
ChoSooMin <soom9611@gmail.com>
2021-01-08 10:49:25 +0900

    [solved] BOJ_11726

--- Baekjoon/Dynamic Programming/BOJ_11726.cpp
+++ Baekjoon/Dynamic Programming/BOJ_11726.cpp
@@ -18,12 +18,13 @@ int dp[1001];
 int main() {
     cin >> n;
     
+    dp[0] = 1; dp[1] = 1;
     dp[2] = 2; dp[3] = 3;
     for (int i = 4; i <= n; i++) {
-        dp[i] = dp[i - 1] + dp[i - 2];
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
     }
     
-    cout << dp[n] % 10007 << endl;
+    cout << dp[n] << endl;
     
     return 0;
 }
