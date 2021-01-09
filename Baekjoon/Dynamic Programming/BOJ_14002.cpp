c461bd1d8cc051e7f1a9a5d2bcad8482ab424ce0
ChoSooMin <soom9611@gmail.com>
2021-01-09 17:16:05 +0900

    [solved] BOJ_14002

--- Baekjoon/Dynamic Programming/BOJ_14002.cpp
+++ Baekjoon/Dynamic Programming/BOJ_14002.cpp
@@ -17,7 +17,7 @@ using namespace std;
 int N;
 int a[1001];
 int dp[1001];
-vector<int> v; // 가장 긴 증가하는 부분 수열
+vector<int> lis; // 가장 긴 증가하는 부분 수열
 
 int main() {
     cin >> N;
@@ -38,25 +38,24 @@ int main() {
         ans = max(ans, dp[i]);
     }
     
-    // leng을 1씩 줄여나가면서,,
+    // 변수 leng에 ans 값을 담고, for문을 통해 dp 배열에 접근한다.
+    // dp 배열에서 dp[i]가 leng과 같은 경우, a[i]는 LIS의 원소에 속한다는 뜻이므로,
+    // lis 벡터에 넣어준다.
     int leng = ans;
-    int inDp = 100;
+    
     for (int i = N - 1; i >= 0; i--) {
         if (dp[i] == leng) {
-            if (a[i] < inDp) {
-                inDp = a[i];
-                v.push_back(inDp);
-                leng--;
-            }
+            lis.push_back(a[i]);
+            leng--;
         }
     }
     
-    reverse(v.begin(), v.end());
+    reverse(lis.begin(), lis.end()); // dp 배열을 뒤에서부터 접근했으므로, lis 벡터를 reverse 시켜준다.
     
     cout << ans << endl;
     
-    for(int i = 0; i < v.size(); i++) {
-        cout << v.at(i) << " ";
+    for(int i = 0; i < lis.size(); i++) {
+        cout << lis.at(i) << " ";
     }
     
     return 0;
