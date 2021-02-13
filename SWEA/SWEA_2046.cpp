4b7ff2eb8b624cd32599313d6b34678cde9c7c6d
ChoSooMin <soom9611@gmail.com>
2021-02-13 23:42:59 +0900

    [solved] SWEA_2046

---
+++ SWEA/SWEA_2046.cpp
@@ -0,0 +1,25 @@
+//
+//  SWEA_2046.cpp
+//  Algorithm
+//
+//  Created by 조수민 on 2021/02/13.
+//  Copyright © 2021 조수민. All rights reserved.
+//
+//  스탬프 찍기(https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=1&contestProbId=AV5QKdT6AyYDFAUq&categoryId=AV5QKdT6AyYDFAUq&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=1&pageSize=10&pageIndex=1&&&&&&&&&&)
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cout << "#";
+    
+    return 0;
+}
