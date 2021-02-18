5cb0ed8bea3346043fb83785094ef6d4c1c96bbe
ChoSooMin <soom9611@gmail.com>
2021-02-18 17:59:29 +0900

    [solved] BOJ_1008

---
+++ Baekjoon/BOJ_1008.cpp
@@ -0,0 +1,24 @@
+//
+//  BOJ_1008.cpp
+//  Algorithm
+//
+//  Created by 조수민 on 2021/02/18.
+//  Copyright © 2021 조수민. All rights reserved.
+//
+//  A/B(https://www.acmicpc.net/problem/1008)
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double a, b;
+    
+    cin >> a >> b;
+    
+    cout.precision(15);
+    cout << a/b;
+    
+    return 0;
+}
