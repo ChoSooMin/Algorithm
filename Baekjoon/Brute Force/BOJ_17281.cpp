db6f11db450fe927d54ede9dc11722648f675256
ChoSooMin <soom9611@gmail.com>
2021-02-25 23:50:31 +0900

    [solved] BOJ_17281

--- Baekjoon/Brute Force/BOJ_17281.cpp
+++ Baekjoon/Brute Force/BOJ_17281.cpp
@@ -9,49 +9,68 @@
 
 #include <stdio.h>
 #include <iostream>
-#include <vector>
-#include <math.h>
+#include <algorithm>
 
 using namespace std;
 
-int batter[10];
-int turn[10];
-
-void play() {
-    int score = 0;
-    
-    
-}
+int N, ans;
+int order[9] = { 3, 0, 1, 2, 4, 5, 6, 7, 8 };
+int norder[9];
+int score[10][55];
+int arr[1515];
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(NULL); cout.tie(NULL);
     
-    // input
-    int N;
     cin >> N;
     
-    vector<int> order = { 1, 2, 3, 5, 6, 7, 8, 9 };
-    sort(order.begin(), order.end());
-    
-    for (int inning = 0; inning < N; inning++) {
-        for (int i = 1; i <= 9; i++) {
-            cin >> batter[i];
-
-            // 게임 진행
-            // 1. next_permutation을 사용해서 순서를 정해준다.
-            do {
-                for (int index = 0; index < order.size(); index++) {
-                    cout << order.at(index) << " ";
-                }
-
-                cout << "\n";
-            } while (next_permutation(order.begin(), order.end()));
-
-            // 2. 각 순서에 맞게 게임을 돌린다.
-            // 3. 그 중 max 값을 출력?
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < 9; j++) {
+            cin >> score[j][i]; // i번째 이닝에 타자 j가 치는 점수
         }
     }
+    
+    do {
+        // 각 순열을 norder에 저장해두기
+        for (int i = 0; i < 9; i++)
+            norder[order[i]] = i;
+            
+        // 매 순열, 얻는 점수를 cnt에 저장한다.
+        int cnt = 0, pos = 0;
+
+        for (int i = 0; i < N; i++) { // N 이닝동안
+            int out = 0; // 각 이닝 초기 out 값은 0으로 시작
+            int prev = pos; // 이닝이 끝나면 다음 이닝은 pos번째 타자부터 시작한다.
+
+            while (out != 3) { // 3 아웃인 경우 다음 이닝으로 넘어간다.
+                int num = score[norder[pos % 9]][i]; // 현재 타자의 값을 가져온다.
+
+                if (num == 0) // 현재 타자가 아웃을 칠 경우
+                    out += 1; // out + 1
+
+                arr[pos++] = num;
+            }
+
+            for (int j = prev; j < pos; j++) {
+                if (!arr[j]) // 만약 내가 아웃이라면 다음으로 넘긴다
+                    continue;
+
+                for (int k = j + 1; k <= j + 3, k < pos; k++) // 1루, 2루, 3루, 홈으로 이루어져있으므로 나는 내 뒤에 3명에게 영향을 받는다
+                    arr[j] += arr[k];
+            }
+        }
+
+        for (int j = 0; j < pos; j++) {
+            if (arr[j] >= 4) // arr값이 4보다 크거나 같다면 해당 주자가 홈으로 들어왔다는 소리이므로 cnt + 1 해준다.
+                cnt += 1;
+        }
+
+        ans = max(ans, cnt);
+        
+    } while (next_permutation(order + 1, order + 9)); // int형 배열 순열 돌리기 (1번 타자는 4번째에 치기로 정해져있으므로, order + 1부터 돌린다)
+    
+    cout << ans << "\n";
     
     return 0;
 }
