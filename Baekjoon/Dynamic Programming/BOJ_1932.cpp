e1b39795028b832b69c896a8e03dbfdce4306ac5
ChoSooMin <soom9611@gmail.com>
2021-02-27 17:56:14 +0900

    [failed] BOJ_1932

--- Baekjoon/Dynamic Programming/BOJ_1932.cpp
+++ Baekjoon/Dynamic Programming/BOJ_1932.cpp
@@ -11,59 +11,58 @@
 #include <iostream>
 #include <queue>
 
-#define MAX 501 // 삼각형의 최대 크기는 500이므로 500 * 501 / 2
-
 using namespace std;
 
-int triangle[MAX][MAX]; // i층에 있는 j번째 숫자
-int dp[MAX][MAX];
 int dy[2] = { 0, 1 };
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(NULL); cout.tie(NULL);
-    
+
     // input
     int n;
     cin >> n;
+
+    int triangle[n + 1][n + 1]; // i층에 있는 j번째 숫자
+    int dp[n + 1][n + 1];
     
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= i; j++) {
             cin >> triangle[i][j];
+            dp[i][j] = triangle[i][j];
         }
     }
-    
+
     /**
      왼쪽 아래 : (i + 1, j)
      오른쪽 아래 : (i + 1, j + 1)
      */
-    
+
     queue<pair<int, int>> Q;
     Q.push({ 1, 1 });
     dp[1][1] = triangle[1][1];
-    
+
     while (!Q.empty()) {
         auto cur = Q.front(); Q.pop();
-        
+
         for (int dir = 0; dir < 2; dir++) {
             int nextI = cur.first + 1;
             int nextJ = cur.second + dy[dir];
-            
+
             if (nextI > n || nextJ > nextI)
                 continue;
-            
+
             dp[nextI][nextJ] = max(dp[cur.first][cur.second] + triangle[nextI][nextJ], dp[nextI][nextJ]);
             Q.push({ nextI, nextJ });
         }
     }
-    
+
     int ans = 0;
     for (int j = 1; j <= n; j++) {
         ans = max(ans, dp[n][j]);
     }
-    
+
     cout << ans << "\n";
-    
-    
+
     return 0;
 }
